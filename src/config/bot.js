ac90d4eb0db1847848852c67fe2fae02d6cc67ac
zippufleet45 <zillaymughalgamer@gmail.com>
2026-05-01 22:58:04 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "all in one bot",
+        name: "rg plays bot",
         // Activity type number (0 = Playing).
         type: 3, 
       },
